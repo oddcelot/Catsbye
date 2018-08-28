799dfe23d6450bef1f1c71b13ac44fc35562a6e1
Stefan Kopco <mail@oddobsessions.com>
2018-08-28 13:28:07 +0200

    Add more sample theme modifaction options

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,13 @@
 import Typography from 'typography'
 import typographyTheme from 'typography-theme-github'
+
 typographyTheme.baseFontSize = '16px'
+
+typographyTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+  h1: {
+    marginBottom: rhythm(3 / 4),
+  },
+})
 
 const typography = new Typography(typographyTheme)
 
